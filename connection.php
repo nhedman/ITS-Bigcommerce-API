b3655cbfa27d589939dbf32750ae6e731417bb83
nhedman <nhedman@intuitsolutions.net>
2014-07-28 17:31:28 -0400

    Persistent curl, additional tools, OO responses
    -cURL is now property, instantiated in __construct
    (significantly improving performance)
    -added load callback public static method
    -all responses now json_decode to stdClass
    -added close method

--- connection.php
+++ connection.php
@@ -16,13 +16,14 @@ class Connection
 	static private $_client;
 	static private $_token;
 	static private $_headers;
+	static private $_curl;
 	private $retries = 0;
 	const RETRY_ATTEMPTS = 5;
 
 
 	/**
-	 * Sets $_hash, $_client, $_token, $_headers upon class instantiation
-	 * 
+	 * Sets $_hash, $_client, $_token, $_headers, $_curl upon class instantiation 
+	 *
 	 * @param $clientId, $storeHash, $token required for the class
 	 * @return void
 	 */
@@ -34,6 +35,12 @@ class Connection
 		$clientHeaderString = 'X-Auth-Client: ' . $this->_client;
 		$tokenHeaderString = 'X-Auth-Token: ' . $this->_token;
 		$this->_headers = array($tokenHeaderString, $clientHeaderString, 'Accept: application/json','Content-Type: application/json');
+		$this->_curl = curl_init();
+		curl_setopt($this->_curl, CURLOPT_HTTPHEADER, $this->_headers);
+		curl_setopt($this->_curl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($this->_curl, CURLOPT_VERBOSE, 1);
+		curl_setopt($this->_curl, CURLOPT_HEADER, 1);
+		curl_setopt($this->_curl, CURLOPT_SSL_VERIFYPEER, false);  
 	}
 
 
@@ -78,6 +85,81 @@ class Connection
 		}
 	}
 
+	/**
+	 * Handle GET request for Load Callback and deep links, should probably be preceded 
+	 * by checking if valid session already exists and followed by adding storeHash and
+	 * accessToken to session.
+	 * No parameters
+	 * 
+	 * @param void
+	 * @return stdClass with fields user.id, user.email, user.store_hash, null if invalid
+	 */
+
+	public static function load() 
+	{
+		$signedRequest = $_GET['signed_payload'];
+		$valid = self::verify($signedRequest);
+		if ($valid) {
+			$storeHash = $valid->store_hash;
+			//valid access request, you should retrieve accessToken from application persistence using $storeHash
+			return $storeHash;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Verify and decode GET request for Load Callback and deep links
+	 * 
+	 * Accepts signed request
+	 * 
+	 * @param $str1, $str2 strings for
+	 * @return stdClass with fields user.id, user.email, user.store_hash null if invalid
+	 */
+
+	public static function verify($signedRequest) {
+		list($payload, $encodedSignature) = explode('.', $signedRequest, 2); 
+
+		// decode the data
+		$signature = base64_decode($encodedSignature);
+		$data = json_decode(base64_decode($payload));
+
+		// confirm the signature
+		$expectedSignature = hash_hmac('sha256', $payload, CLIENT_SECRET, $raw = true);
+	
+		if (secureCompare($signature, $expectedSignature)) {
+			return null;
+		}
+
+		return $data;
+	}
+
+	/**
+	 * Time-invariant comparison of strings to foil timing attacks
+	 * 
+	 * Accepts strings for comparison
+	 * 
+	 * @param $str1, $str2 strings for
+	 * @return boolean, false if not identical
+	 */
+
+	static function secureCompare($str1, $str2) {
+		$res = $str1 ^ $str2;
+		$ret = strlen($str1) ^ strlen($str2); //not the same length, then fail ($ret != 0)
+		for($i = strlen($res) - 1; $i >= 0; $i--) {
+			$ret += ord($res[$i]);
+		}
+		return !$ret;
+	}
+
+	/**
+	 * Implementation of http_parse_headers for PHP without PECL
+	 * 
+	 * Accepts string response header
+	 * 
+	 * @param $header, string response header
+	 * @return array
+	 */
 
 	public static function http_parse_headers( $header )
     {
@@ -96,16 +178,34 @@ class Connection
         return $retVal;
     }
 
+	/**
+	 * Controls rate of request to API based on response headers
+	 * 
+	 * Accepts array response headers
+	 * 
+	 * @param $headers, array response headers
+	 * @return boolean, false (with sleep) if rate limit currently reached, true if limit ok
+	 */
+
     public function rate_limit( $headers )
-    {	if ( isset($headers['X-Retry-After']) ) {
-    		if ($headers['X-Retry-After'] > 0) {
-        		sleep($headers['X-Retry-After']);
-        		return false;
-        	}
+    {
+    	if (@$headers['X-Retry-After'] > 0) {
+        	sleep($headers['X-Retry-After']);
+			return false;
         }
+      	
         $this->retries = 0;
         return true;
     }
+
+	/**
+	 * Lodges an error based on current request
+	 * 
+	 * Accepts the response body, url, json encoded information and request type
+	 * 
+	 * @param $body, response body, $url, request url, $json, $json encoded response, $type request type
+	 * @return void, sets array $error
+	 */
 
     public function error($body, $url, $json, $type) {
     	global $error;
@@ -131,7 +231,7 @@ class Connection
 	 * Accepts the resource to perform the request on
 	 * 
 	 * @param $resource string $resource a string to perform get on
-	 * @return results or var_dump error
+	 * @return stdClass results or var_dump error
 	 */
 	public function get($resource, array $filter = null) {
 
@@ -140,14 +240,8 @@ class Connection
 			$url .= '?' . http_build_query($filter);
 		}
 
-		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_URL, $url);
-		curl_setopt($curl, CURLOPT_HTTPHEADER, $this->_headers);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($curl, CURLOPT_VERBOSE, 1);
-		curl_setopt($curl, CURLOPT_HEADER, 1);
-		curl_setopt($curl, CURLOPT_HTTPGET, 1);
-		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);            
+		curl_setopt($this->_curl, CURLOPT_URL, $url);
+		curl_setopt($this->_curl, CURLOPT_HTTPGET, 1);          
 		$response = curl_exec($curl);
 		$http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 		$header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
@@ -158,7 +252,6 @@ class Connection
 			$this->retries = $this->retries + 1;
 			return $this->get($resource, $filter);
 		}
-		curl_close ($curl);
 		if ($http_status == 200) {
 			$results = json_decode($body);
 			return $results;
@@ -176,22 +269,16 @@ class Connection
 	 * 
 	 * @param string $resource a string to perform get on
 	 * @param array $fields an array to be sent in the request
-	 * @return results or var_dump error
+	 * @return stdClass results or var_dump error
 	 */
 	public function put($resource, $fields) {
 			
 		$url = 'https://api.bigcommerce.com/stores/' . $this->_hash . '/v2/' . $resource;
 		$json = json_encode($fields);
-		
-		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_URL, $url);
-		curl_setopt($curl, CURLOPT_HTTPHEADER, $this->_headers);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($curl, CURLOPT_VERBOSE, 1);
-		curl_setopt($curl, CURLOPT_HEADER, 1);
-		curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
-		curl_setopt($curl, CURLOPT_POSTFIELDS, $json); 
-		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+		curl_setopt($this->_curl, CURLOPT_URL, $url);
+		curl_setopt($this->_curl, CURLOPT_CUSTOMREQUEST, "PUT");
+		curl_setopt($this->_curl, CURLOPT_POSTFIELDS, $json); 
 		$response = curl_exec($curl);
 		$http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 		$header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
@@ -202,7 +289,6 @@ class Connection
 			$this->retries = $this->retries + 1;
 			return $this->put($resource, $fields);
 		}
-		curl_close($curl);
 		if ($http_status == 200) {
 			$results = json_decode($body);
 			return $results;
@@ -219,22 +305,16 @@ class Connection
 	 * 
 	 * @param string $resource a string to perform get on
 	 * @param array $fields an array to be sent in the request
-	 * @return results or var_dump error
+	 * @return stdClass results or var_dump error
 	 */
 	public function post($resource, $fields) {
 		global $error;
 		$url = 'https://api.bigcommerce.com/stores/' . $this->_hash . '/v2/' . $resource;
 		$json = json_encode($fields);
 
-		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_URL, $url);
-		curl_setopt($curl, CURLOPT_HTTPHEADER, $this->_headers);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($curl, CURLOPT_VERBOSE, 1);
-		curl_setopt($curl, CURLOPT_HEADER, 1);
-		curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
-		curl_setopt($curl, CURLOPT_POSTFIELDS, $json);
-		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($this->_curl, CURLOPT_URL, $url);
+		curl_setopt($this->_curl, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($this->_curl, CURLOPT_POSTFIELDS, $json);
 		$response = curl_exec ($curl);
 		$http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 		$header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
@@ -245,7 +325,6 @@ class Connection
 			$this->retries = $this->retries + 1;
 			return $this->post($resource, $fields);
 		}
-		curl_close ($curl);
 		if ($http_status == 201) {
 			$results = json_decode($body);
 			return $results;
@@ -260,20 +339,14 @@ class Connection
 	 * Accepts the resource to perform the request on
 	 * 
 	 * @param string $resource a string to perform get on
-	 * @return proper response or var_dump error
+	 * @return string response or var_dump error
 	 */
 	public function delete($resource) {
 			
 		$url = 'https://api.bigcommerce.com/stores/' . $this->_hash . '/v2/' . $resource;
 		
-		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_URL, $url);
-		curl_setopt($curl, CURLOPT_HTTPHEADER, $this->_headers);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($curl, CURLOPT_VERBOSE, 1);
-		curl_setopt($curl, CURLOPT_HEADER, 1);
-		curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
-		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($this->_curl, CURLOPT_URL, $url);
+		curl_setopt($this->_curl, CURLOPT_CUSTOMREQUEST, "DELETE");
 		$response = curl_exec($curl);
 		$http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 		$header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
@@ -290,6 +363,19 @@ class Connection
 		 } else {
 		 	$this->error($body, $url, null, 'DELETE');
 		 }
+	}
+
+	/**
+	 * Close curl connection
+	 * 
+	 * Void
+	 * 
+	 * @param void
+	 * @return void
+	 */
+
+	public function close() {
+		curl_close($this->_curl);
 	}
 }
 
